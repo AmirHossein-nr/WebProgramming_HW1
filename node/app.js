67d2142ab5e33c9d51e34aa33ef79a443ada937e
AmirHossein_Razlighi <79264971+AmirHossein-nr@users.noreply.github.com>
2021-10-26 00:39:59 +0330

    Create app.js

---
+++ node/app.js
@@ -0,0 +1,29 @@
+const crypto = require("crypto");
+const redis = require("redis");
+const express = require("express");
+const app = express();
+const client = redis.createClient({
+    host:"localhost",
+    port:"6379",
+    password:""
+});
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.route("ip/node/sha256")
+    .get(async (req, res) => {
+        let userString = await client.get(req.param.hash);
+        if (userString === null) 
+            res.status(404).send(404);
+        else res.send(JSON.stringify({"message": userString}));
+    })
+    .post(async (req, res) =>{
+        let userString = req.body["message"];
+        let hash = crypto.createHash("sha256").update(`${userString}`).digest('hex');
+        await client.set(hash, userString);
+    });
+app.route("*")
+    .get((req, res) => res.status(404).send(404))
+    .post((req, res) => res.status(404).send(404));
+    
+app.listen(3000);
