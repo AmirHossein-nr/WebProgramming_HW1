e06211521d5d03e43f51cf4592be6ce0e20b44b1
hooman <hm.keshvari@gmail.com>
2021-10-26 03:28:39 +0330

    im tired

--- locust/load.py
+++ locust/load.py
@@ -3,4 +3,4 @@ from locust import HttpUser, task
 class HelloWorldUser(HttpUser):
     @task
     def load_index(self):
-        self.client.get("/ip/go/sha256?hash=1677238712")
+        self.client.get("/ip/node/sha256?hash=66c3f0ff5b53c7ba2e6c53ea2be3e7a1d5e1fb3e3ae9d9a211e1e898bb43eb00")
--- node/app.js
+++ node/app.js
@@ -2,6 +2,7 @@ const crypto = require("crypto");
 const redis = require("redis");
 const express = require("express");
 const app = express();
+const port =3000
 const client = redis.createClient({
     host:"localhost",
     port:"6379",
@@ -10,17 +11,23 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.route("/ip/node/sha256")
     .get(async (req, res) => {
-        let userString = await client.get(req.param.hash);
-        if (userString === null) 
-            res.status(404).send(404);
-        else res.send(JSON.stringify({"message": userString}));}  
-    )
+        console.log(req.query.hash)
+        // let userString = await client.get(req.param.hash);
+        // if (userString === null) 
+        //     res.status(404).send(404);
+        // else res.send(JSON.stringify({"message": userString}));
+        res.status(200).send(200)
+    })
     .post(async (req, res) =>{
         let userString = req.body["message"];
         let hash = crypto.createHash("sha256").update(`${userString}`).digest('hex');
         await client.set(hash, userString);
+        res.status(200).send(200)
     });
 app.route("*")
     .get((req, res) => res.status(404).send(404))
     .post((req, res) => res.status(404).send(404));
-app.listen(3000);
+app.listen(port,()=>{
+    console.log(`listening at port ${port}`)
+})
+    
